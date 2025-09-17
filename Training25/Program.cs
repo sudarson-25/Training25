02b1a531e1087eba82bc8898c2a65da41baf5b3d
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-17 16:27:07 +0530

    Initial commit

---
+++ Training25/Program.cs
@@ -0,0 +1,13 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program on main branch.
+// ------------------------------------------------------------------------------------------------
+namespace Training25;
+internal class Program {
+   static void Main (string[] args) {
+      Console.WriteLine ("Hello, World!");
+   }
+}
