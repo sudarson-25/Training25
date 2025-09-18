d5ad709e519da6a6b5378262cfa97c41466290fd
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-18 15:12:06 +0530

    T01: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,17 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// program to convert a given decimal number to different forms (Hexadecimal, Binary)
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Console.Write ("\nNumber Conversion Game\n~~~~~~~~~~~~~~~~~~~~~~\nInput: ");
+         Console.WriteLine (int.TryParse (Console.ReadLine (), out int decValue)
+            ? $"HEX: {decValue:X}\nBinary: {Convert.ToString (decValue, 2)}"
+            : "Invalid Input");
+         Console.Write ("Press 'Y' to continue: ");
+      } while (Console.ReadLine () is "Y" or "y");
    }
 }
