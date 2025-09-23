bf0caaef34f08a1053c2867bab93341d1f29097b
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-23 10:44:57 +0530

    Added new method to perform conversion

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,17 +3,29 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// program to convert a given decimal number to different forms (Hexadecimal, Binary)
+// Program to convert a given decimal number to different forms (Hexadecimal, Binary)
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
 internal class Program {
    static void Main (string[] args) {
       do {
          Console.Write ("\nNumber Conversion Game\n~~~~~~~~~~~~~~~~~~~~~~\nInput: ");
-         Console.WriteLine (int.TryParse (Console.ReadLine (), out int decValue)
-            ? $"HEX: {decValue:X}\nBinary: {Convert.ToString (decValue, 2)}"
-            : "Invalid Input");
+         if (int.TryParse (Console.ReadLine (), out int decValue)) {
+            var bin = Convert.ToString (decValue, 2);
+            Console.WriteLine ($"HEX: {decValue:X}\nBinary: {bin}");
+            Console.WriteLine (DecToBin (decValue) == bin ? "DecToBin() method works!"
+               : "DecToBin() method fails!");
+         } else
+            Console.WriteLine ("Invalid Input!");
          Console.Write ("Press 'Y' to continue: ");
       } while (Console.ReadLine () is "Y" or "y");
+   }
+
+   static string DecToBin (int decValue) {
+      if (decValue == 0) return "0";
+      char[] bits = new char[32];
+      for (int i = 31; i >= 0; i--)
+         bits[31 - i] = ((decValue & (1 << i)) != 0) ? '1' : '0';
+      return new string (bits);
    }
 }
