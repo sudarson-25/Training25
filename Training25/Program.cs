c127a19c24e0cc2dbacec8953e2e2589279f54a5
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-26 11:55:13 +0530

    T5: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,19 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to print multiplication tables from 1 to 10 in a tabular form
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Console.Write ("\nMultiplication Tables\n~~~~~~~~~~~~~~~~~~~~~\nEnter a number: ");
+         if (int.TryParse (Console.ReadLine (), out int num))
+            for (int i = 1; i <= 10; i++)
+               Console.WriteLine ($"{num} * {i,2} = {(long)num * i}");
+         else Console.WriteLine ("Invalid input!");
+         Console.Write ("Press 'Y' to continue: ");
+      } while (Console.ReadLine () is "y" or "Y");
    }
 }
