ed8f31a6b78bf1d5ad70220e0be07c1ff99c121e
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-26 12:56:40 +0530

    T6: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,23 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to find the digital root of a given number.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      do {
+         Console.Write ("\nDigital Root\n~~~~~~~~~~~~\nEnter a number: ");
+         if (int.TryParse (Console.ReadLine (), out int num)) {
+            Console.WriteLine (num < 0 ? "Digital root is undifined!" : DigitalRoot (num));
+         } else Console.WriteLine ("Invalid input!");
+         Console.Write ("Press 'Y' to continue: ");
+      } while (Console.ReadLine () is "y" or "Y");
+   }
+
+   static int DigitalRoot (int num) {
+      if (num == 0) return 0;
+      return 1 + (num - 1) % 9;
    }
 }
