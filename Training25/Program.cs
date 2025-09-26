d789ba1b7f739e9ce46caab31e3dceb689185d73
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-26 17:23:05 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -9,13 +9,11 @@ namespace Training25;
 
 internal class Program {
    static void Main (string[] args) {
-      do {
-         Console.Write ("\nMultiplication Tables\n~~~~~~~~~~~~~~~~~~~~~\nEnter a number: ");
-         if (int.TryParse (Console.ReadLine (), out int num))
-            for (int i = 1; i <= 10; i++)
-               Console.WriteLine ($"{num} * {i,2} = {(long)num * i}");
-         else Console.WriteLine ("Invalid input!");
-         Console.Write ("Press 'Y' to continue: ");
-      } while (Console.ReadLine () is "y" or "Y");
+      Console.WriteLine ("Multiplication Tables\n~~~~~~~~~~~~~~~~~~~~~\n");
+      for (int i = 1; i <= 10; i++) {
+         for (int j = 1; j <= 10; j++)
+            Console.WriteLine ($"{i} * {j,2} = {i * j}");
+         Console.WriteLine ();
+      }
    }
 }
