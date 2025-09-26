6990cf0719fdbc54e683345d2ce9f65d0246d3e2
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-26 16:48:36 +0530

    Added a method to calculate LCM

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -10,17 +10,14 @@ namespace Training25;
 internal class Program {
    static void Main (string[] args) {
       do {
-         Console.Write ("\nLCM and GCD Generator\n~~~~~~~~~~~~~~~~~~~~~\nEnter the first number : ");
+         Console.Write ("\nLCM and GCD Generator\n~~~~~~~~~~~~~~~~~~~~~\n" +
+            "Enter the first number : ");
          if (int.TryParse (Console.ReadLine (), out int num) && num != int.MinValue) {
             Console.Write ("Enter the second number: ");
-            if (int.TryParse (Console.ReadLine (), out int num1) && num1 != int.MinValue) {
-               if (num == 0 && num1 == 0) Console.WriteLine ("GCD and LCM are undefined when both the inputs are 0");
-               else {
-                  int gcd = GCD (num, num1);
-                  Console.WriteLine ($"GCD                    : {gcd}\nLCM                    : " +
-                     $"{(long)Math.Abs (num) * Math.Abs (num1) / gcd}");
-               }
-            } else Console.WriteLine ("Second number is invalid");
+            Console.WriteLine (int.TryParse (Console.ReadLine (), out int num1) &&
+               num1 != int.MinValue ? num == 0 && num1 == 0 ? "GCD and LCM are undefined when " +
+               "both the inputs are 0" : $"GCD                    : {GCD (num, num1)}\nLCM      " +
+               $"              : {LCM (num, num1)}" : "Second number is invalid");
          } else Console.WriteLine ("First number is invalid!");
          Console.Write ("Press 'Y' to continue  : ");
       } while (Console.ReadLine () is "y" or "Y");
@@ -30,11 +27,15 @@ internal class Program {
    static int GCD (int num, int num1) {
       num = Math.Abs (num);
       num1 = Math.Abs (num1);
+      int temp;
       while (num1 != 0) {
-         int temp = num1;
+         temp = num1;
          num1 = num % num1;
          num = temp;
       }
       return num;
    }
+
+   /// <summary>Returns the LCM of the two given integers</summary>
+   static long LCM (int num, int num1) => (long)Math.Abs (num) * Math.Abs (num1) / GCD (num, num1);
 }
