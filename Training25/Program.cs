a99c5d852a3daa234cdd0c7dfc6f1f2273b658a5
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-29 12:20:03 +0530

    Review Fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,23 +6,24 @@
 // Program to convert a given decimal number to different forms (Hexadecimal, Binary)
 // ------------------------------------------------------------------------------------------------
 using System.Text;
+using static System.Console;
 
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       do {
-         Console.Write ("\nNumber Conversion Game\n~~~~~~~~~~~~~~~~~~~~~~\nInput : ");
-         if (int.TryParse (Console.ReadLine (), out int decValue)) {
-            string bin = Convert.ToString (decValue, 2), hex = decValue.ToString ("X");
-            Console.WriteLine ($"HEX   : {hex}\nBinary: {bin}");
-            Console.WriteLine (DecToBin (decValue) == bin ? "DecToBin() method works!"
+         Write ("\nNumber Conversion Game\n~~~~~~~~~~~~~~~~~~~~~~\nInput : ");
+         if (int.TryParse (ReadLine (), out int decValue)) {
+            string bin = decValue.ToString ("B"), hex = decValue.ToString ("X");
+            WriteLine ($"HEX   : {hex}\nBinary: {bin}");
+            WriteLine (DecToBin (decValue) == bin ? "DecToBin () method works!"
                : "DecToBin() method fails!");
-            Console.WriteLine (DecToHex (decValue) == hex ? "DecToHex() method works!"
+            WriteLine (DecToHex (decValue) == hex ? "DecToHex () method works!"
                : "DecToHex() method fails!");
-         } else Console.WriteLine ("Invalid Input!");
-         Console.Write ("Press 'Y' to continue: ");
-      } while (Console.ReadLine () is "Y" or "y");
+         } else WriteLine ("Invalid Input!");
+         Write ("Press 'Y' to continue: ");
+      } while (ReadLine () is "Y" or "y");
    }
 
    /// <summary>Returns the binary value of the given decimal number</summary>
@@ -31,9 +32,7 @@ internal class Program {
       var sb = new StringBuilder (32);
       for (int i = 31; i >= 0; i--)
          sb.Append (((decValue & (1 << i)) != 0) ? '1' : '0');
-      int idx = 0;
-      while (sb[idx] == '0') idx++;
-      return sb.ToString (idx, 32 - idx);
+      return TrimLeadingZeros (sb, 32);
    }
 
    /// <summary>Returns the hexadecimal value of the given decimal number</summary>
@@ -41,12 +40,15 @@ internal class Program {
       if (decValue == 0) return "0";
       var sb = new StringBuilder ("00000000");
       for (int i = 7; i >= 0; i--)
-         sb[7 - i] = sHexChars[(decValue >> (i * 4)) & 0xF];
-      int idx = 0;
-      while (sb[idx] == '0') idx++;
-      return sb.ToString (idx, 8 - idx);
+         sb[7 - i] = sHex[(decValue >> (i * 4)) & 0xF];
+      return TrimLeadingZeros (sb, 8);
    }
 
-   static char[] sHexChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
-      'E', 'F'];
+   static string TrimLeadingZeros (StringBuilder sb, int size) {
+      int idx = 0;
+      while (sb[idx] == '0') idx++;
+      return sb.ToString (idx, size - idx);
+   }
+
+   static string sHex = "0123456789ABCDEF";
 }
