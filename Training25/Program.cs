7939bf6ff7d841372d3100f1ed372dac36a72528
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-29 14:38:49 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,62 +3,65 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program to display a given number in words and roman numerals
+// Program to display a given number in words and Roman numerals
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       do {
-         Console.Write ("\nMenu\n~~~~~\n1.Display in words\n2.Display in roman numerals (0<x<400" +
-            "0)\nEnter your choice: ");
-         if (int.TryParse (Console.ReadLine (), out int choice))
-            switch (choice) {
-               case 1:
-                  Console.Write ("Enter the number : ");
-                  if (int.TryParse (Console.ReadLine (), out int num)) {
+         Write ("\nEnter the choice of display\n1.Words\n2.Roman numerals (0 to 4000)\n" +
+            "Enter your choice: ");
+         if (int.TryParse (ReadLine (), out int choice) && choice == 1 || choice == 2) {
+            Write ("Enter the number : ");
+            if (int.TryParse (ReadLine (), out int num))
+               switch (choice) {
+                  case 1:
                      string numInWords = NumberToWords (num);
-                     Console.WriteLine ($"\n{num} - {char.ToUpper (numInWords[0]) +
+                     WriteLine ($"\n{num} - {char.ToUpper (numInWords[0]) +
                         numInWords[1..]}");
-                  } else Console.WriteLine ("\nInvalid Input!");
-                  break;
-               case 2:
-                  Console.Write ("Enter the number : ");
-                  // Roman numerals are usually written for numbers from 1 to 3999
-                  if (int.TryParse (Console.ReadLine (), out int num1) && num1 > 0 && num1 < 4000)
-                     Console.WriteLine ($"\n{num1} - {NumberToRoman (num1)}");
-                  else Console.WriteLine ("\nInvalid Input!");
-                  break;
-               default: Console.WriteLine ("\nInvalid choice!"); break;
-            } else Console.WriteLine ("\nInvalid Input!");
-         Console.Write ("Press 'Y' to continue: ");
-      } while (Console.ReadLine () is "y" or "Y");
+                     break;
+                  case 2:
+                     // Roman numerals are usually written for numbers from 1 to 3999
+                     WriteLine (num > 0 && num < 4000 ? $"\n{num} - {NumberToRoman (num)}" :
+                        "\nInput out of range!");
+                     break;
+                  default: break;
+               } else WriteLine ("\nInvalid Input!");
+         } else WriteLine ("\nInvalid choice!");
+         Write ("Press 'Y' to continue: ");
+      } while (ReadLine () is "y" or "Y");
    }
 
-   /// <summary>Returns the given integer in words</summary>
+   // Returns the given integer in words
    static string NumberToWords (int num) {
       string numInWords = "";
       if (num == 0) return "Zero";
       if (num < 0) numInWords = "Minus ";
       if (num == int.MinValue)
          return numInWords += NumberToWords (int.MaxValue / 10 * 10) + "-" + NumberToWords (8);
-      int absNum = Math.Abs (num);
-      foreach (var (value, label) in sValuesMap)
-         if (absNum / value > 0) {
-            numInWords += NumberToWords (absNum / value) + label;
-            absNum %= value;
+      num = Math.Abs (num);
+      foreach (var (value, label) in sValuesMap) {
+         int quotient = num / value;
+         if (quotient > 0) {
+            numInWords += NumberToWords (quotient) + label;
+            num %= value;
          }
-      if (num > 0 && numInWords != "") numInWords += "and ";
-      if (absNum < 20) numInWords += sUnitsMap[absNum];
+      }
+      if (num > 0 && numInWords != "" && numInWords != "Minus ")
+         numInWords += "and ";
+      if (num < 20) numInWords += sUnits[num];
       else {
-         numInWords += tensMap[absNum / 10];
-         int digit = absNum % 10;
-         if (digit > 0) numInWords += "-" + sUnitsMap[digit];
+         numInWords += sTens[num / 10];
+         int digit = num % 10;
+         if (digit > 0) numInWords += "-" + sUnits[digit];
       }
       return numInWords.TrimEnd ();
    }
 
-   /// <summary>Returns the roman numeral of the given integer</summary>
+   // Returns the roman numeral of the given integer
    static string NumberToRoman (int number) {
       string result = "";
       foreach (var (value, symbol) in sNumerals)
@@ -73,8 +76,8 @@ internal class Program {
       (1000, " thousand "), (100, " hundred ")], sNumerals = [ (1000, "M"), (900, "CM"),
          (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"),
          (9, "IX"), (5, "V"), (4, "IV"), (1, "I") ];
-   static string[] sUnitsMap = [ "", "one", "two", "three", "four", "five", "six", "seven",
+   static string[] sUnits = [ "", "one", "two", "three", "four", "five", "six", "seven",
             "eight", "nine", "ten", "eleven", "twelve", "thirteen","fourteen", "fifteen", "sixteen"
-      , "seventeen", "eighteen", "nineteen" ], tensMap = [ "", "", "twenty", "thirty", "forty",
+      , "seventeen", "eighteen", "nineteen" ], sTens = [ "", "", "twenty", "thirty", "forty",
          "fifty", "sixty", "seventy", "eighty", "ninety" ];
 }
