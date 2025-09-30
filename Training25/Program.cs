708f92cfad6c0f09e342ea6f83515bc6f3233a13
Sudarson <sudarson.selvakumar@trumpf.com>
2025-09-30 10:11:16 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,20 +5,28 @@
 // Program.cs
 // Program to find the digital root of a given number.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       do {
-         Console.Write ("\nDigital Root\n~~~~~~~~~~~~\nEnter a number: ");
-         Console.WriteLine (int.TryParse (Console.ReadLine (), out int num) ?
-            (num < 0 ? "Digital root is undifined!" : DigitalRoot (num)) : "Invalid input!");
-         Console.Write ("Press 'Y' to continue: ");
-      } while (Console.ReadLine () is "y" or "Y");
+         Write ("\nDigital Root\n~~~~~~~~~~~~\nEnter a number: ");
+         if (int.TryParse (ReadLine (), out int num)) {
+            if (Validate (num)) WriteLine ($"Digital Root  : {DigitalRoot (num)}");
+         } else WriteLine ("Invalid input!");
+         Write ("Press 'Y' to continue: ");
+      } while (ReadLine () is "y" or "Y");
    }
 
    static int DigitalRoot (int num) {
       if (num == 0) return 0;
       return 1 + (num - 1) % 9;
+   }
+
+   static bool Validate (int num) {
+      if (num < 0) { WriteLine ("Digital root is undefined!"); return false; }
+      return true;
    }
 }
