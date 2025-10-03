14ecc0e089324f326496b081e61a93b6a3c5ffcd
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-03 12:56:16 +0530

    T9: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -3,11 +3,30 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program to reduce a string of lowercase characters by deleting a pair of adjacent letters that
+// match.
 // ------------------------------------------------------------------------------------------------
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      do {
+         Console.WriteLine ("\nReduced String\n~~~~~~~~~~~~~~");
+         string? input;
+         do {
+            Console.Write ("Enter a lowercase string: ");
+            input = Console.ReadLine ();
+            if (string.IsNullOrWhiteSpace (input)) Console.WriteLine ("Invalid input!");
+         } while (string.IsNullOrWhiteSpace (input));
+         if (input.All (char.IsLower)) {
+            Stack<char> stack = new ();
+            foreach (char letter in input)
+               if (stack.Count > 0 && stack.Peek () == letter) stack.Pop ();
+               else stack.Push (letter);
+            string reducedString = new ([.. stack.Reverse ()]);
+            Console.WriteLine ("Reduced String          : " + reducedString);
+         } else Console.WriteLine ("String must contain only lowercase characters");
+         Console.Write ("Press 'Y' to continue   : ");
+      } while (Console.ReadLine () is "y" or "Y");
    }
 }
