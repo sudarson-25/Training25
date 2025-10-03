0802ac576b5651319a275d9c2f8548a9fa7fd83b
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-03 09:07:20 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -6,6 +6,7 @@
 // Program to print Pascal's triangle.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
+
 namespace Training25;
 
 internal class Program {
@@ -13,27 +14,20 @@ internal class Program {
       do {
          Write ("\nPascal's Triangle\n~~~~~~~~~~~~~~~~~\nEnter number of rows (1 to 34): ");
          if (int.TryParse (ReadLine (), out int rows) && rows > 0 && rows <= 34) {
-            List<int[]> values = PascalValues (rows);
+            List<int[]> values = [];
             for (int row = 0; row < rows; row++) {
+               int[] rowEntries = new int[row + 1];
                Write ("".PadLeft (rows - row));
-               for (int col = 0; col < row + 1; col++) Write (values[row][col] + " ");
+               for (int col = 0; col <= row; col++) {
+                  rowEntries[col] = col == 0 || col == row ? 1 : values[row - 1][col - 1] +
+                     values[row - 1][col];
+                  Write (rowEntries[col] + " ");
+               }
+               values.Add (rowEntries);
                WriteLine ();
             }
          } else WriteLine ("Invalid input!");
          Write ("\nPress 'y' to continue: ");
       } while (ReadLine () is "y" or "Y");
-   }
-
-   // Returns a list of integer arrays containing the values of Pascal's triangle.
-   static List<int[]> PascalValues (int rows) {
-      List<int[]> values = [];
-      for (int row = 0; row < rows; row++) {
-         int[] rowEntries = new int[row + 1];
-         for (int col = 0; col <= row; col++)
-            rowEntries[col] = col == 0 || col == row ? 1 : values[row - 1][col - 1] +
-               values[row - 1][col];
-         values.Add (rowEntries);
-      }
-      return values;
    }
 }
