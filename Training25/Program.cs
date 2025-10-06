29873d1aaa830ba8194905304afc0b04cd9f295f
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-06 11:50:17 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,17 +12,15 @@ namespace Training25;
 internal class Program {
    static void Main () {
       do {
-         Write ("\nArmstrong Number\n~~~~~~~~~~~~~~~~\nEnter a number: ");
+         Write ("Armstrong Number\n~~~~~~~~~~~~~~~~\nEnter a number: ");
          if (int.TryParse (ReadLine (), out int num) && num >= 0) {
-            int numCopy = num, numCopy1 = num, power = 0;
-            double arm = 0;
+            int numCopy = num, power = num.ToString ().Length;
+            double armstrong = 0;
             while (numCopy > 0)
-               (power, numCopy) = (power + 1, numCopy / 10);
-            while (numCopy1 > 0)
-               (arm, numCopy1) = (arm + Math.Pow (numCopy1 % 10, power), numCopy1 / 10);
-            WriteLine (arm == num ? "Armstrong Number!" : "Not an Armstrong number ");
+               (armstrong, numCopy) = (armstrong + Math.Pow (numCopy % 10, power), numCopy / 10);
+            WriteLine (armstrong == num ? "Armstrong Number!" : "Not an Armstrong number ");
          } else WriteLine ("Invalid input!");
-         Write ("Press 'Y' to continue: ");
-      } while (ReadLine () is "y" or "Y");
+         WriteLine ("Press 'Y' to continue");
+      } while (ReadKey (true).Key is ConsoleKey.Y);
    }
 }
