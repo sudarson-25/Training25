66087bd84577934fcc9fbf5ad0254c12b2df12f7
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-06 09:08:04 +0530

    Minor fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,36 +5,38 @@
 // Program.cs
 // Program to check whether the password is strong or not with reasons.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 
 internal class Program {
    static void Main () {
       do {
-         Console.WriteLine ("\nStrong Password\n~~~~~~~~~~~~~~~");
+         WriteLine ("\nStrong Password\n~~~~~~~~~~~~~~~");
          string? password;
          do {
-            Console.Write ("Enter a password: ");
-            password = Console.ReadLine ();
-            if (string.IsNullOrWhiteSpace (password)) Console.WriteLine ("Invalid Input!\n");
+            Write ("Enter a password: ");
+            password = ReadLine ();
+            if (string.IsNullOrWhiteSpace (password)) WriteLine ("Invalid Input!\n");
          } while (string.IsNullOrWhiteSpace (password));
          int passwordLength = password.Length;
          bool hasDigit = password.Any (char.IsDigit), hasLower = password.Any (char.IsLower),
             hasUpper = password.Any (char.IsUpper), hasSpecialChar =
             password.Any (sSpecialCharacters.Contains);
          if (passwordLength < 6)
-            Console.WriteLine ("Must be atleast 6 characters in length");
+            WriteLine ("Must be atleast 6 characters in length");
          if (!hasDigit)
-            Console.WriteLine ("Must conatin atleast one digit");
+            WriteLine ("Must conatin atleast one digit");
          if (!hasLower)
-            Console.WriteLine ("Must conatin atleast one lowercase English character");
+            WriteLine ("Must conatin atleast one lowercase English character");
          if (!hasUpper)
-            Console.WriteLine ("Must conatin atleast one uppercase English character");
+            WriteLine ("Must conatin atleast one uppercase English character");
          if (!hasSpecialChar)
-            Console.WriteLine ("Must conatin atleast one special character");
-         Console.WriteLine (passwordLength >= 6 && hasDigit && hasLower && hasUpper &&
+            WriteLine ("Must conatin atleast one special character");
+         WriteLine (passwordLength >= 6 && hasDigit && hasLower && hasUpper &&
             hasSpecialChar ? "Strong Password!" : "Weak Password!");
-         Console.Write ("\nPress 'Y' to continue: ");
-      } while (Console.ReadLine () is "y" or "Y");
+         Write ("\nPress 'Y' to continue: ");
+      } while (ReadLine () is "y" or "Y");
    }
 
    static string sSpecialCharacters = "!@#$%^&*()-+";
