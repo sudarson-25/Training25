7866f9c7a9362b2fef2489c5c3f28749624f8d9e
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-06 09:15:39 +0530

    Minor fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,17 +5,19 @@
 // Program.cs
 // Program that takes a string and returns the reversed string.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
 
 internal class Program {
    static void Main () {
       do {
-         Console.WriteLine ("\nReverse the String\n~~~~~~~~~~~~~~~~~~");
+         WriteLine ("\nReverse the String\n~~~~~~~~~~~~~~~~~~");
          string? input;
          do {
-            Console.Write ("Enter a string : ");
-            input = Console.ReadLine ();
-            if (string.IsNullOrWhiteSpace (input)) Console.WriteLine ("Invalid input!");
+            Write ("Enter a string : ");
+            input = ReadLine ();
+            if (string.IsNullOrWhiteSpace (input)) WriteLine ("Invalid input!");
          } while (string.IsNullOrWhiteSpace (input));
          List<char> rev = [.. input.Replace (" ", "").Reverse ()];
          for (int i = 0; i < input.Length; i++) {
@@ -23,7 +25,7 @@ internal class Program {
             if (char.IsLower (input[i])) rev[i] = char.ToLower (rev[i]);
             if (char.IsUpper (input[i])) rev[i] = char.ToUpper (rev[i]);
          }
-         Console.Write ("Reversed string: " + string.Concat (rev) + "\nPress 'Y' to continue: ");
-      } while (Console.ReadLine () is "y" or "Y");
+         Write ("Reversed string: " + string.Concat (rev) + "\nPress 'Y' to continue: ");
+      } while (ReadLine () is "y" or "Y");
    }
 }
