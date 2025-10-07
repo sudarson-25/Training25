19708404a1f76b115a39b3af411d711f4c8ce0b9
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-07 14:28:08 +0530

    T12: Initial commit

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,9 +5,24 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
+
 namespace Training25;
+
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      while (true) {
+         Write ("Voting Contest\n~~~~~~~~~~~~~~\nEnter a string: ");
+         var S = ReadLine ();
+         if (string.IsNullOrWhiteSpace (S)) { WriteLine ("Invalid input!"); continue; }
+         if (!S.All (char.IsLetter)) { WriteLine ("Invalid input!"); continue; }
+         char winner = GetWinner (S);
+         WriteLine ($"{char.ToUpper (winner)} or {char.ToLower (winner)}\nPress 'Y' to continue");
+         if (ReadKey (true).Key is not ConsoleKey.Y) break;
+      }
    }
+
+   // Returns the winner of the voting contest
+   static char GetWinner (string S) => S.GroupBy (c => c).OrderByDescending (g => g.Count ())
+      .ThenBy (g => S.IndexOf (g.Key)).First ().Key;
 }
