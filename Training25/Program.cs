5cf62c320026b6a8dc79b6172fac6265f58fd97f
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-08 11:59:41 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -5,20 +5,18 @@
 // Program.cs
 // Program to print the nth Armstrong number (assuming base 10).
 // ------------------------------------------------------------------------------------------------
-using System; //Needed since the program is run from the cmd prompt
 using static System.Console;
 
 namespace Training25;
 
 internal class Program {
    static void Main (string[] args) {
-      if (args.Length != 1 || !int.TryParse (args[0], out int input) || input <= 0 || input > 32)
-         WriteLine ("Invalid input!");
-      else {
-         int armstrongCount = 0, num = 0;
-         while (armstrongCount < input) { if (IsArmstrong (num)) armstrongCount++; num++; }
-         WriteLine ($"Nth Armstrong number : {num - 1}");
+      if (args.Length != 1 || !int.TryParse (args[0], out int input) || input <= 0 || input > 32) {
+         WriteLine ("Invalid input!"); return;
       }
+      int armstrongCount = 0, num = 0;
+      while (armstrongCount < input) { if (IsArmstrong (num)) armstrongCount++; num++; }
+      WriteLine ($"Nth Armstrong number : {num - 1}");
    }
 
    // Returns true if the number is an Armstrong number else returns false
