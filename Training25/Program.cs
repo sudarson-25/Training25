1e750d113b5299a20927dfefbebd0895f8eb2025
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-15 17:10:23 +0530

    Review fix 7

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -15,10 +15,8 @@ internal class Program {
       while (true) {
          WriteLine ("Sort and Swap Special Characters\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
          string input = GetValidString ("Enter the letters: ");
-         GetValidChar ("Enter the special character: ", char.ToLower, out char specialCharacter);
-         GetValidChar ("\nType 'A' for ascending order or 'D' for" +
-            " descending order: ", input => input is 'A' or 'a' ? "ascending" :
-            "descending", out string order);
+         GetValidChar ("Enter the special character: ", out char specialCharacter);
+         GetValidChar ("\nType 'a' for ascending order: ", out char order);
          var output = SortAndSwap ([.. input], specialCharacter, order);
          WriteLine ($"\nLetters: {input}\nSorted : {string.Join (", ", output)}\nPress 'Y' to" +
             " continue");
@@ -26,7 +24,18 @@ internal class Program {
       }
    }
 
-   //Prints the prompt message and returns the validated input from the user
+   // Prints the prompt message and returns the validated input from the user
+   static void GetValidChar (string prompt, out char value) {
+      while (true) {
+         Write (prompt);
+         value = ReadKey ().KeyChar;
+         if (!char.IsLetter (value)) { PrintError (); continue; }
+         value = char.ToLower (value);
+         break;
+      }
+   }
+
+   // Prints the prompt message and returns the validated input from the user
    static string GetValidString (string prompt) {
       while (true) {
          Write (prompt);
@@ -39,26 +48,15 @@ internal class Program {
       }
    }
 
-   //Prints the prompt message and returns the validated input from the user
-   static void GetValidChar<T> (string prompt, Func<char, T> Convert, out T value) {
-      while (true) {
-         Write (prompt);
-         var input = ReadKey ().KeyChar;
-         if (!char.IsLetter (input)) { PrintError (); continue; }
-         value = Convert (input);
-         break;
-      }
-   }
-
-   //Prints an error message
+   // Prints an error message
    static void PrintError () => WriteLine ("Invalid input!");
 
    // Sorts the given list by keeping the elements matching the given character to the last of the
    // list based on the given order
-   static List<char> SortAndSwap (List<char> inputList, char specialChar, string order) {
+   static List<char> SortAndSwap (List<char> inputList, char specialChar, char order) {
       int count = inputList.RemoveAll (ch => ch == specialChar);
-      inputList = order is "descending" ? [.. inputList.OrderDescending ()] :
-         [.. inputList.Order ()];
+      inputList = order is 'a' ? [.. inputList.Order ()] :
+         [.. inputList.OrderDescending ()];
       for (int i = 0; i < count; i++) inputList.Add (specialChar);
       return inputList;
    }
