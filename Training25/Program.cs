11ef80bbafcbe76454817223d125060ce0e79c2a
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-15 10:27:48 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -7,6 +7,7 @@
 // identical digits.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
+using static System.Math;
 
 namespace Training25;
 
@@ -22,24 +23,15 @@ internal class Program {
 
    // Returns the smallest transform steps
    static int GetSmallestTransform (int num) {
-      List<int> digits = [.. num.ToString ().Select (a => a - 48)],
-         distinctDigits = [.. digits.Distinct ()];
-      distinctDigits.Sort ();
-      int steps1 = StepsCalculator (digits, distinctDigits[distinctDigits.Count / 2]);
-      List<(int, int)> frequency = [];
-      foreach (int distinctDigit in distinctDigits)
-         frequency.Add ((digits.Count (digit => digit == distinctDigit), distinctDigit));
-      frequency.Sort ();
-      frequency.Reverse ();
-      int steps2 = StepsCalculator (digits, frequency[0].Item2);
-      return steps1 < steps2 ? steps1 : steps2;
-
-      // Returns the number of steps required to convert all digits to the reference digit
-      static int StepsCalculator (List<int> digits, int reference) {
-         int steps = 0;
-         foreach (int number in digits)
-            steps += number >= reference ? number - reference : reference - number;
-         return steps;
+      List<int> digits = [.. num.ToString ().Select (a => a - '0')],
+         distinctDigits = [.. digits.Distinct ().Order ()];
+      int steps1 = digits.Sum (digit => Abs (digit - distinctDigits[distinctDigits.Count / 2]));
+      (int count, int digit) reference = (0, 0);
+      foreach (int distinctDigit in distinctDigits) {
+         int count = digits.Count (digit => digit == distinctDigit);
+         if (count > reference.count) reference = (count, distinctDigit);
       }
+      int steps2 = digits.Sum (digit => Abs (digit - reference.digit));
+      return steps1 < steps2 ? steps1 : steps2;
    }
 }
