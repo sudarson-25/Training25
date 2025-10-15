ce3e3a85f1e7eaaaa13c2abca6b4636e01d39aa7
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-15 15:24:40 +0530

    Review fix 2

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -15,23 +15,20 @@ internal class Program {
    static void Main () {
       while (true) {
          Write ("Smallest Transform\n~~~~~~~~~~~~~~~~~~~~\nEnter a number: ");
-         if (!int.TryParse (ReadLine (), out int num)) { WriteLine ("Invalid Input!"); continue; }
-         WriteLine ($"{GetSmallestTransform (num)} steps\nPress 'Y' to continue");
+         if (!int.TryParse (ReadLine (), out int num) || num < 0) {
+            WriteLine ("Invalid Input!"); continue;
+         }
+         WriteLine ($"{GetSmallestTransform (num, out string result)} steps\nTransformed number:" +
+            $" {result}\nPress 'Y' to continue");
          if (ReadKey (true).Key is not ConsoleKey.Y) break;
       }
    }
 
    // Returns the smallest transform steps
-   static int GetSmallestTransform (int num) {
-      List<int> digits = [.. num.ToString ().Select (a => a - '0')],
-         distinctDigits = [.. digits.Distinct ().Order ()];
-      int steps1 = digits.Sum (digit => Abs (digit - distinctDigits[distinctDigits.Count / 2]));
-      (int count, int digit) reference = (0, 0);
-      foreach (int distinctDigit in distinctDigits) {
-         int count = digits.Count (digit => digit == distinctDigit);
-         if (count > reference.count) reference = (count, distinctDigit);
-      }
-      int steps2 = digits.Sum (digit => Abs (digit - reference.digit));
-      return steps1 < steps2 ? steps1 : steps2;
+   static int GetSmallestTransform (int num, out string result) {
+      List<int> digits = [.. num.ToString ().Select (a => a - '0').Order ()];
+      int count = digits.Count, median = digits[count / 2];
+      result = new ((char)('0' + median), count);
+      return digits.Sum (digit => Abs (digit - median));
    }
 }
