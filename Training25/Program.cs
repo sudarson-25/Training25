ff939d8beea37f06410df0c4c0effd4e23669fe7
Sudarson <sudarson.selvakumar@trumpf.com>
2025-10-17 11:56:27 +0530

    Review fix

--- Training25/Program.cs
+++ Training25/Program.cs
@@ -12,63 +12,57 @@ internal class Program {
    static void Main () {
       MyList<int> list1 = new ();
       for (int i = 10; i <= 100; i += 10) list1.Add (i);
-      list1.Display ();
+      list1.Print ();
       WriteLine (list1.Remove (50) ? "Element removed" : "Element not found");
-      list1.Display ();
+      list1.Print ();
       WriteLine (list1[8]);
       list1.Insert (4, 9);
-      list1.Display ();
+      list1.Print ();
       list1.RemoveAt (9);
-      list1.Display ();
+      list1.Print ();
       list1.Clear ();
-      list1.Display ();
+      list1.Print ();
    }
 
    #region class MyList ---------------------------------------------------------------------------
    /// <summary>This class implements a custom list using arrays as the underlying data structure.
    /// </summary>
    class MyList<T> {
+      #region Constructor --------------------------------------------
       // Constructor to allocate the initial array
       public MyList () {
          mArray = new T[4];
          mCount = 0;
       }
+      #endregion
+
+      #region Properties ---------------------------------------------
+      // Gets the current capacity of the underlying array
+      public int Capacity => mArray.Length;
 
       // Gets the number of elements in the list
       public int Count => mCount;
 
-      // Gets the current capacity of the underlying array
-      public int Capacity => mArray.Length;
-
       // Indexer to get or set elements at a specific index
       public T this[int index] {
          get {
-            ArgumentOutOfRangeException.ThrowIfNegative (index);
-            if (index >= mCount) throw new IndexOutOfRangeException ();
+            ValidateArgument (index);
+            ValidateIndex (index);
             return mArray[index];
          }
          set {
-            ArgumentOutOfRangeException.ThrowIfNegative (index);
-            if (index >= mCount) throw new IndexOutOfRangeException ();
+            ValidateArgument (index);
+            ValidateIndex (index);
             mArray[index] = value;
          }
       }
+      #endregion
 
+      #region Methods ------------------------------------------------
       // Adds the given element to the end of the list
       public void Add (T element) {
          if (mCount == Capacity) Array.Resize (ref mArray, Capacity * 2);
          mArray[mCount++] = element;
-      }
-
-      // Removes the first occurrence of the given element from the list
-      public bool Remove (T element) {
-         int index = Array.IndexOf (mArray, element);
-         if (index is -1) return false;
-         else {
-            for (int i = index; i < mCount - 1; i++) mArray[i] = mArray[i + 1];
-            mCount--;
-            return true;
-         }
       }
 
       // Clears the list
@@ -80,7 +74,7 @@ internal class Program {
 
       // Inserts the given element at the specified index
       public void Insert (int index, T a) {
-         ArgumentOutOfRangeException.ThrowIfNegative (index);
+         ValidateArgument (index);
          if (index > mCount) throw new IndexOutOfRangeException ();
          if (mCount == Capacity) Array.Resize (ref mArray, Capacity * 2);
          for (int i = mCount; i > index; i--) mArray[i] = mArray[i - 1];
@@ -88,19 +82,34 @@ internal class Program {
          mCount++;
       }
 
+      // Displays the elements of the list
+      public void Print () {
+         for (int i = 0; i < mCount; i++) Write (this[i] + " ");
+         WriteLine ();
+      }
+
+      // Removes the first occurrence of the given element from the list
+      public bool Remove (T element) {
+         int index = Array.IndexOf (mArray, element);
+         if (index is -1) return false;
+         RemoveAt (index);
+         return true;
+      }
+
       // Removes the element at the specified index
       public void RemoveAt (int index) {
-         ArgumentOutOfRangeException.ThrowIfNegative (index);
-         if (index >= mCount) throw new IndexOutOfRangeException ();
+         ValidateArgument (index);
+         ValidateIndex (index);
          for (int i = index; i < mCount - 1; i++) mArray[i] = mArray[i + 1];
          mCount--;
       }
 
-      // Displays the elements of the list
-      public void Display () {
-         for (int i = 0; i < mCount; i++) Write (this[i] + " ");
-         WriteLine ();
+      void ValidateArgument (int index) => ArgumentOutOfRangeException.ThrowIfNegative (index);
+
+      void ValidateIndex (int index) {
+         if (index >= mCount) throw new IndexOutOfRangeException ();
       }
+      #endregion
 
       #region Private data -------------------------------------------
       T[] mArray;
